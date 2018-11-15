4efeb2ce0274060747895052900d3b4e3629d004
liushuang <269620154@qq.com>
2018-11-15 22:17:01 +0800

    2018/11/15

---
+++ globalmem/.tmp_versions/globalmem.mod
@@ -0,0 +1,3 @@
+/home/liushuang/workspace/study/linux_driver/globalmem/globalmem.ko
+/home/liushuang/workspace/study/linux_driver/globalmem/globalmem.o
+
--- globalmem/globalmem.c
+++ globalmem/globalmem.c
@@ -23,7 +23,10 @@ struct globalmem_dev
 {
 	struct cdev cdev;//内核自带的字符设备
 	unsigned char mem[MEM_SIZE];  //开辟的内存空间大小
+	unsigned int current_len;  //当前fifo中有效数据长度
 	struct mutex my_mutex;  //互斥体，应为本驱动会使用可能阻塞的函数，所以使用互斥体而不是自旋锁
+	wait_queue_head_t r_wait; //读等待队列头
+	wait_queue_head_t w_wait; //写等待队列头
 };
 
 struct globalmem_dev *globalmem_devp;  //设备结构体指针，后面会赋值给私有数据使用
@@ -36,24 +39,54 @@ static ssize_t globalmem_read(struct  file *filp, char __user *buf, size_t size,
 	unsigned int count  = size;
 	int ret = 0;
 	struct globalmem_dev  *dev = filp->private_data;
-
-	if(p >= MEM_SIZE)
-		return 0;
-	if(count > MEM_SIZE - p) //最大读取数据量
-		count  = MEM_SIZE - p;
+	DECLARE_WAITQUEUE(wait,current);  //初始化定义一个等待队列的元素wait
+	add_wait_queue(&dev->r_wait,&wait);  //添加元素到读等待队列
 
 	mutex_lock(&dev->my_mutex);
-	if(copy_to_user(buf,dev->mem + p, count))
+
+	while(dev->current_len == 0)  //当前fifo长度为0，无数可读，即需要阻塞等待
+	{
+		if(filp->f_flags & O_NONBLOCK)  //如果设置了非阻塞标志位，没读到数据就直接返回
+		{
+			ret = -EAGAIN;
+			goto out;
+		}
+		__set_current_state(TASK_INTERRUPTIBLE); //改变当前进程状态为可中断的睡眠状态，达到阻塞等待的目的
+		mutex_unlock(&dev->my_mutex);
+
+		schedule();  //开启调度，CPU将调度到其他进程
+		if(signal_pending(current))
+		{
+			ret = -ERESTARTSYS;
+			goto out2;
+		}
+
+		mutex_lock(&dev->my_mutex);
+	}
+
+	if(count > dev->current_len)
+		count = dev->current_len;
+
+
+	if(copy_to_user(buf,dev->mem, count))  //拷贝数据到应用层
 	{
 		return -EFAULT;
+		goto out;
 	}
-	else{
+	else
+	{
+		memcpy(dev->mem, dev->mem + count, dev->current_len - count);
+		dev->current_len -= count; //当前可读的fifo长度
+	}
 
-		*ppos += count;
-		ret = count;
-		printk(KERN_INFO"read data ok!\n");
-	}
+	wake_up_interruptible(&dev->w_wait);  //读出数据后，FIFO有空间来写入，所以唤醒写操作的等待队列
+	ret = count;
+
+	out:
 	mutex_unlock(&dev->my_mutex);
+	out2:
+	remove_wait_queue(&dev->r_wait,&wait);
+	set_current_state(TASK_RUNNING);
 
 	return ret;
 }
@@ -65,24 +98,51 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,size_t 
 	int ret = 0;
 	struct globalmem_dev *dev = filp->private_data;
 
-	if(p >= MEM_SIZE)
-		return 0;
-	if(count > MEM_SIZE - p)
-		count = MEM_SIZE -p;
+	DECLARE_WAITQUEUE(wait, current); //初始化定义一个wait等待队列元素
 
-	mutex_lock(&dev->my_mutex); //获取互斥体
-	if(copy_from_user(dev->mem, buf, count))
+	mutex_lock(&dev->my_mutex);
+	add_wait_queue(&dev->w_wait,&wait);
+
+	while(dev->current_len == MEM_SIZE)  //FIFO空间已满，无空间可写，需要阻塞
+	{
+		if(filp->f_flags & O_NONBLOCK)
+		{
+			ret = -EAGAIN;
+			goto out;
+		}
+
+		__set_current_state(TASK_INTERRUPTIBLE);
+		mutex_unlock(&dev->my_mutex);
+
+		schedule();
+		if(signal_pending(current))
+		{
+			ret = -ERESTARTSYS;
+			goto out2;			
+		}
+
+		mutex_lock(&dev->my_mutex);
+	}
+	if(count > MEM_SIZE - dev->current_len)
+			count = MEM_SIZE - dev->current_len;
+
+	if(copy_from_user(dev->mem + dev->current_len, buf, count))
 	{
 		return -EFAULT;
+		goto out;
 	}
 	else
 	{
-		*ppos += count;
-		ret = count;
+		dev->current_len = dev->current_len + count;
 		printk(KERN_INFO"write data ok!\r\n");
+		wake_up_interruptible(&dev->r_wait);  //写数据完成，唤醒读操作
+		ret = count;
 	}
-
-	mutex_unlock(&dev->my_mutex); //写完成，释放互斥体
+	out:
+	mutex_unlock(&dev->my_mutex);
+	out2:
+	remove_wait_queue(&dev->w_wait,&wait);
+	set_current_state(TASK_RUNNING);
 	return ret;
 }
 
@@ -202,16 +262,18 @@ static int __init globalmem_init(void)
 		goto fail_malloc;
 
 	}
-	mutex_init(&globalmem_devp->my_mutex);  //初始化设备的互斥体 
 	for(i = 0; i<DEVICE_NUM; i++)
 		globalmem_setup_cdev(globalmem_devp + i,i);
+
+	mutex_init(&globalmem_devp->my_mutex);  //初始化设备的互斥体 
+	init_waitqueue_head(&globalmem_devp->r_wait); //初始化等待队列头
+	init_waitqueue_head(&globalmem_devp->w_wait);
 	return  0;
 
 fail_malloc:
 	unregister_chrdev_region(devno,1);
 	return ret;
 }
-
 module_init(globalmem_init);
 
 static void __exit globalmem_exit(void)
