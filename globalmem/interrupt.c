58453d390c007ecb4332cbe3a1028900107f44f3
liushuang <269620154@qq.com>
2018-12-03 23:00:28 +0800

    增加中断的笔记2018/1203

--- globalmem/interrupt.c
+++ globalmem/interrupt.c
@@ -1,4 +1,21 @@
 /*************字符设备驱动模板---内核中断和时钟操作**********************/
+/************************************
+ *
+ * 中断底半部主要用法
+ * 1.tasklet(不允许睡眠)
+ * void my_tasklet_func(unsigned long);
+ * DECLARE_TASKLET(my_tasklet,my_tasklet_func,data);
+ * tasklet_schedule(&my_tasklet); //调度tasklet
+ *
+ * 2.工作队列（可以睡眠）
+ * struct work_struct my_wq;   //定义一个工作队列
+ * void my_wq_func(struct work_struct *work);// 定义一个处理函数
+ * INIT_WORK(&my_wq, my_wq_func);   //初始化并绑定处理函数
+ * schdeule_work(&my_wq);      //调度工作队列
+ *
+ *4.软中断(不可以睡眠)
+ *
+ * /
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
